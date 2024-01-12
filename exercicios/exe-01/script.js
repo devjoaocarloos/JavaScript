bdfc1f27385927be88bb44f7933cb31cda2a3d5b
devjoaocarlos <devjoaocarlos@outlook.com>
2024-01-12 18:54:34 -0300

    usando javascript numa pagina we

---
+++ exercicios/exe-01/script.js
@@ -0,0 +1,9 @@
+let a = Number(prompt("Digite o valor de a"));
+let b = Number(prompt("Digite o valor de b"));
+
+let soma = a + b;
+let subtr = a - b;
+let mult = a * b;
+let div = a / b;
+
+console.log(soma, subtr, mult, div);
