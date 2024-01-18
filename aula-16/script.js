a32151e5e59eb23d435ff0907c230689435cc5e2
devjoaocarlos <devjoaocarlos@outlook.com>
2024-01-18 13:43:39 -0300

    exercicio - 02: finalizado.

---
+++ aula-16/script.js
@@ -0,0 +1,25 @@
+// let a = 5;
+// let b = 4;
+
+// let soma = a + b;
+
+// if (a + b === 9) {
+//     console.log('A resposta é ' + soma);
+// }
+
+// const idade = prompt("Informe a sua idade:");
+
+// if (idade >= 18) {
+//   alert("Vc é um adulto");
+// } else if (idade >= 12 && idade < 18) {
+//   alert("Vc é adolecente");
+// } else if (idade < 12) {
+//   alert("Vc é crianca");
+// } else {
+//   alert("Nn sei oq vc pode ser");
+// }
+
+
+const resultado = (6 === 6) ? 'Verdadeiro' : 'Falso'
+
+console.log(resultado);
---
+++ exercicios/exe-02/calculo de dano/script.js
@@ -0,0 +1,25 @@
+const atacante = prompt("Qual o nome do personagem atacante?");
+const poderDeAtaque = parseFloat(prompt("Qual é o seu poder de ataque?"));
+
+const defensor = prompt("Qual o nome do personagem defensor?");
+let pontosDeVida = parseFloat(prompt("Quantos pontos de vida ele possui?")); 
+const poderDeDefesa = parseFloat(prompt("Qual é o seu poder de defesa?"));
+const possuiEscudo = prompt("Ele possui escudo? (Sim/Não)");
+
+let danoCausado = 0;
+
+if (poderDeAtaque > poderDeDefesa && possuiEscudo === "Não") {
+  danoCausado = poderDeAtaque - poderDeDefesa;
+} else if (poderDeAtaque > poderDeDefesa && possuiEscudo === "Sim") {
+  danoCausado = (poderDeAtaque - poderDeDefesa) / 2;
+} 
+
+pontosDeVida -= danoCausado
+
+alert(atacante + " casou " + danoCausado + " pontos de dano em " + defensor);
+
+alert(
+  atacante + "\n Poder de ataque: " + poderDeAtaque + "\n\n" + 
+  defensor + "\n Pontos de vida: " + pontosDeVida + 
+  "\n Poder de defesa " + poderDeDefesa + "\n Possui escudo: " + possuiEscudo
+)
