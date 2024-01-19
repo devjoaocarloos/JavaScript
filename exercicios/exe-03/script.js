6cfab79af039f69e97abc81aa08e774b4d2c9b12
devjoaocarlos <devjoaocarlos@outlook.com>
2024-01-19 16:15:52 -0300

    exercicio 03 - finalizado

---
+++ exercicios/exe-03/script.js
@@ -0,0 +1,33 @@
+const medida = parseFloat(prompt("Insira uma medida em metros:"));
+const unidade = prompt(
+  "Para qual unidade desejar converter?" +
+    "\n1 - milímetro (mm)" +
+    "\n2 - centímetro (cm)" +
+    "\n3 - decímetro (dm)" +
+    "\n4 - decâmetro (dam)" +
+    "\n5 - hectômetro (hm)" +
+    "\n6 - quilômetro (km)"
+);
+
+switch (unidade) {
+  case "1":
+    alert("Resulatdo: " + medida + "m = " + medida * 1000 + "mm");
+    break;
+  case "2":
+    alert("Resultado: " + medida + "m = " + medida * 100 + "cm");
+    break;
+  case "3":
+    alert("Resultado: " + medida + "m = " + medida * 10 + "dm");
+    break;
+  case "4":
+    alert("Resultado: " + medida + "m = " + medida / 10 + "dam");
+    break;
+  case "5":
+    alert("Resultado: " + medida + "m = " + medida / 100 + "hm");
+    break;
+  case "6":
+    alert("Resultado: " + medida + "m = " + medida / 1000 + "km");
+    break;
+  default:
+    alert("Opção Inválida");
+}
