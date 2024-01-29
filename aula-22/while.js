bd45fc35492271b9a1683fbb0f0f41a6afb38698
devjoaocarlos <devjoaocarlos@outlook.com>
2024-01-29 14:03:01 -0300

    aula-22

---
+++ aula-22/while.js
@@ -0,0 +1,9 @@
+let velocidade = 100;
+
+while (velocidade > 0) {
+  console.log("O carro esta a " + velocidade + " km/h");
+  velocidade -= 20;
+  console.log("Diminuindo 20km/h");
+}
+
+console.log("O carro parou");
